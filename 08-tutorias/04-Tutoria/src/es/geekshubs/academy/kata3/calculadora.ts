654989b9ebaa52f837e9310012c32e65f95b60d8
vicboma1 <victorbolinchesmarin@gmail.com>
2020-07-09 18:16:54 +0200

    añadida la resta

---
+++ 08-tutorias/04-Tutoria/src/es/geekshubs/academy/kata3/calculadora.ts
@@ -0,0 +1,7 @@
+export const suma = (a:number, b:number) : number => {
+    return a + b 
+}
+
+export const resta = (a:number, b:number) : number => {
+    return a - b 
+}
---
+++ 08-tutorias/04-Tutoria/test/es/geekshubs/academy/kata3/uni/suite.test.ts
@@ -0,0 +1,43 @@
+import { suma } from "../../../../../../src/es/geekshubs/academy/kata3/calculadora";
+import { resta } from "../../../../../../src/es/geekshubs/academy/kata3/calculadora";
+
+describe('Testing for kata3', function () {
+
+    test("Calculadora Operación suma: 0 + 0 = 0", ()=> {   
+        const expected = 0
+        const result = suma(0,0)
+        expect(result).toBe(expected)
+    });
+
+    test("Calculadora Operación suma: 10 + 10 = 20", ()=> {   
+        const expected = 20
+        const result = suma(10,10)
+        expect(result).toBe(expected)
+    });
+
+    test("Calculadora Operación suma: -1 + 0 = -1", ()=> {   
+        const expected = -1
+        const result = suma(-1,0)
+        expect(result).toBe(expected)
+    });
+
+    test("Calculadora Operación resta: 0 - 0 = 0", ()=> {   
+        const expected = 0
+        const result = resta(0,0)
+        expect(result).toBe(expected)
+    });
+
+    test("Calculadora Operación resta: 3 - 2 = 1", ()=> {   
+        const expected = 1
+        const result = resta(3,2)
+        expect(result).toBe(expected)
+    });
+
+    test("Calculadora Operación resta: -3 - 2 = -5", ()=> {   
+        const expected = -5
+        const result = resta(-3,2)
+        expect(result).toBe(expected)
+    });
+    
+});
+
